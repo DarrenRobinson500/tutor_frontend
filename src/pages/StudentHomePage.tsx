783ef08217ee4fe1b6410c101f42d300d8adc8bd
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-02-08 12:37:13 +1100

    Calendar updates and navbar fix

--- src/pages/StudentHomePage.tsx
+++ src/pages/StudentHomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { WeeklyCalendar } from "./components/WeeklyCalendar";
