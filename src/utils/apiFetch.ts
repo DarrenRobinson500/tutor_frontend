94508c982c585d47f50d48a0d37ff9522c59ebd3
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-03-04 13:05:11 +1100

    Get online working = change from hardcoding to env variables

--- src/utils/apiFetch.ts
+++ src/utils/apiFetch.ts
@@ -1,7 +1,7 @@
 export async function apiFetch(url: string, options: any = {}) {
 //   const API_URL = "http://localhost:8000";
-//   const API_URL = (process.env.REACT_APP_API_URL ?? "").replace(/\/$/, "");
-  const API_URL = "https://web-production-f1310.up.railway.app";
+  const API_URL = (process.env.REACT_APP_API_URL ?? "").replace(/\/$/, "");
+//   const API_URL = "https://web-production-f1310.up.railway.app";
 
   const fullUrl = `${API_URL}${url}`;
 
